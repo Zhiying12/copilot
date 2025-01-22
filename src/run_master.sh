ee47c85d70a21181c840258d8dbd12188237b35f
Ubuntu <ubuntu@ip-172-31-22-119.us-west-2.compute.internal>
2025-01-22 21:40:04 +0000

    update scripts for running

--- src/run_master.sh
+++ src/run_master.sh
@@ -1,1 +1,21 @@
-bin/master -N=3 -twoLeaders=true
+serverPort=$1
+
+pids=()
+
+cleanup() {
+    for pid in "${pids[@]}"; do
+        kill "$pid"
+    done
+    rm *.txt
+}
+
+trap cleanup SIGINT
+
+bin/master -N=2 -twoLeaders=true &
+pids+=($!)
+
+bin/server -maddr=172.31.22.119 -addr=172.31.22.119 -port=${serverPort} -e=false -p=8 -copilot=true -exec=true -dreply=true &
+
+pids+=($!)
+
+wait
--- src/run_server.sh
+++ src/run_server.sh
@@ -1,3 +1,4 @@
 serverPort=$1
 
-bin/server -port=${serverPort} -e=false -p=8 -copilot=true
+bin/server -maddr=172.31.22.119 -addr=172.31.18.42 -port=${serverPort} -e=false -p=8 -copilot=true -exec=true -dreply=true
+
